fa5c184b15e14fe645d80a1ec79aaa20432009ee
sscstack <shashanktech2@gmail.com>
2023-02-08 21:18:59 +0530

    Added find-all and findby-id

--- src/main/java/com/example/erpapp/controller/ERPController.java
+++ src/main/java/com/example/erpapp/controller/ERPController.java
@@ -2,9 +2,11 @@ package com.example.erpapp.controller;
 
 import com.example.erpapp.model.Student;
 import com.example.erpapp.service.ERPService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,10 @@ public class ERPController {
     @GetMapping("find-all-students")
     public List<Student> findAllStudents() {
         return erpService.findAll();
+    }
+
+    @GetMapping("find-student/rollno/{rollno}")
+    public Student findStudentById(@PathVariable int rollno) {
+        return erpService.findById(rollno);
     }
 }
--- src/main/java/com/example/erpapp/service/ERPService.java
+++ src/main/java/com/example/erpapp/service/ERPService.java
@@ -1,10 +1,12 @@
 package com.example.erpapp.service;
 
 import com.example.erpapp.model.Student;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ERPService {
 
     private static final List<Student> students=new ArrayList<>();
@@ -13,13 +15,32 @@ public class ERPService {
     static {
         Student s1=new Student(++rollno,"abc","male",
                 List.of(80.0,90.0,87.0,60.0),"Delhi");
+        students.add(s1);
         Student s2=new Student(++rollno,"xyz","male",
                 List.of(60.0,66.0,87.7,61.6),"Haryana");
+
+        students.add(s2);
         Student s3=new Student(++rollno,"Anshika","female",
                 List.of(88.0,99.0,30.0,20.0),"Mumbai");
+        students.add(s3);
         Student s4=new Student(++rollno,"sarah","female",
                 List.of(60.0,62.1,87.9,61.0),"Chennai");
+        students.add(s4);
         Student s5=new Student(++rollno,"san","male",
                 List.of(79.64,99.0,55.0,45.0),"Rajasthan");
+
+        students.add(s5);
+    }
+
+    public List<Student> findAll() {
+        return students;
+    }
+
+    public Student findById(int rollno) {
+        for(Student student:students) {
+            if(student.getRollNumber()==rollno) return student;
+        }
+
+        return null;
     }
 }
